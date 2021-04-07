9a7d8033c674153dfbc776bcaafbc92b30c54c71
Luciano Lacurcia <lucho.lacurcia@gmail.com>
2021-04-07 00:00:46 -0300

    Agregado servicio cron

--- main.js
+++ main.js
@@ -1,69 +1,72 @@
+const cron = require('node-cron');
 const datosPersonales = require('./datos.json');
 const puppeteer = require('puppeteer');
 const { PendingXHR } = require('pending-xhr-puppeteer');    // https://www.npmjs.com/package/pending-xhr-puppeteer
 
-(async () => {
-    const browser = await puppeteer.launch( { headless: false });   // agregar lo de headless...
+cron.schedule('* * */8 * * *', function(){
+	(async () => {
+		const browser = await puppeteer.launch( { headless: false });   // agregar lo de headless...
 
-    const page = await browser.newPage();
-    const pendingXHR = new PendingXHR(page);
+		const page = await browser.newPage();
+		const pendingXHR = new PendingXHR(page);
 
-    // Ir a la pagina inicial y seleccionar el lugar y el tramite
-    await page.goto('https://agendaweb.udelar.edu.uy');
-    await pendingXHR.waitForAllXhrFinished();
-    await page.select("#formSeleccion\\:agendas", "FING")   //  HAY QUE ESCAPAR A LOS CARACTERES ESPECIALES LPM "\"
-    await pendingXHR.waitForAllXhrFinished();
-    await page.select("#formSeleccion\\:recursos > select", "BibliotecaSalaDeLectura")
-    await pendingXHR.waitForAllXhrFinished();
-	await Promise.all([
-		page.waitForNavigation(),
-		page.click("#formSeleccion\\:linkAgendarReserva")
-	])
-    await pendingXHR.waitForAllXhrFinished();
+		// Ir a la pagina inicial y seleccionar el lugar y el tramite
+		await page.goto('https://agendaweb.udelar.edu.uy');
+		await pendingXHR.waitForAllXhrFinished();
+		await page.select("#formSeleccion\\:agendas", "FING")   //  HAY QUE ESCAPAR A LOS CARACTERES ESPECIALES LPM "\"
+		await pendingXHR.waitForAllXhrFinished();
+		await page.select("#formSeleccion\\:recursos > select", "BibliotecaSalaDeLectura")
+		await pendingXHR.waitForAllXhrFinished();
+		await Promise.all([
+			page.waitForNavigation(),
+			page.click("#formSeleccion\\:linkAgendarReserva")
+		])
+		await pendingXHR.waitForAllXhrFinished();
 
-	// traigo todos los elementos que tienen la clase diaConCupo (o sea son días del calendario disponibles)
-	let texts = await page.evaluate(() => {
-		let data = [];
-		let elements = document.getElementsByClassName('diaConCupo');
-		for (var element of elements) {
-			var datos = { id: element.id, contenido: parseInt(element.textContent) };
-			data.push(datos);
-		}
-		return data;
-	});
-	texts.forEach(dia => console.log("id: " + dia.id + " contenido: " + dia.contenido ));
-	// acá busco el dia disponible mas cercano (la agenda no disponibiliza días ya pasados)
-	var contenido = texts.reduce((min, p) => p.contenido < min ? p.contenido : min, texts[0].contenido);
-	var res = "#" + texts.find(element => element.contenido = contenido).id;
-	console.log(res);
+		// traigo todos los elementos que tienen la clase diaConCupo (o sea son días del calendario disponibles)
+		let texts = await page.evaluate(() => {
+			let data = [];
+			let elements = document.getElementsByClassName('diaConCupo');
+			for (var element of elements) {
+				var datos = { id: element.id, contenido: parseInt(element.textContent) };
+				data.push(datos);
+			}
+			return data;
+		});
+		texts.forEach(dia => console.log("id: " + dia.id + " contenido: " + dia.contenido ));
+		// acá busco el dia disponible mas cercano (la agenda no disponibiliza días ya pasados)
+		var contenido = texts.reduce((min, p) => p.contenido < min ? p.contenido : min, texts[0].contenido);
+		var res = "#" + texts.find(element => element.contenido = contenido).id;
+		console.log(res);
 
-	await Promise.all([
-		page.waitForNavigation(),
-		await page.click(res)
-	])
-    await pendingXHR.waitForAllXhrFinished();
+		await Promise.all([
+			page.waitForNavigation(),
+			await page.click(res)
+		])
+		await pendingXHR.waitForAllXhrFinished();
 
 
-	// elijo opción matutina:
-	await Promise.all([
-		page.waitForNavigation(),
-		await page.click("#tablaMatutina\\:0\\:linkReservarMatutina")
-	])
+		// elijo opción matutina:
+		await Promise.all([
+			page.waitForNavigation(),
+			await page.click("#tablaMatutina\\:0\\:linkReservarMatutina")
+		])
 
-    await pendingXHR.waitForAllXhrFinished();
-	//	ingreso mis datos:
-	console.log(datosPersonales);
-	await page.type("#ci", datosPersonales.CedulaSinDigito);
-	await page.type("#digito", datosPersonales.DigitoVerificador);
-	await page.type("#ape", datosPersonales.Apellido);
-	await page.type("#nom", datosPersonales.Nombre);
-	await page.type("#tel", datosPersonales.Telefono);
-	await page.type("#mail", datosPersonales.Email);
-	await page.type("#horario", datosPersonales.Horario);
+		await pendingXHR.waitForAllXhrFinished();
+		//	ingreso mis datos:
+		console.log(datosPersonales);
+		await page.type("#ci", datosPersonales.CedulaSinDigito);
+		await page.type("#digito", datosPersonales.DigitoVerificador);
+		await page.type("#ape", datosPersonales.Apellido);
+		await page.type("#nom", datosPersonales.Nombre);
+		await page.type("#tel", datosPersonales.Telefono);
+		await page.type("#mail", datosPersonales.Email);
+		await page.type("#horario", datosPersonales.Horario);
 
-	await page.click("#botonConfirmar")
-    await pendingXHR.waitForAllXhrFinished();
+		await page.click("#botonConfirmar")
+		await pendingXHR.waitForAllXhrFinished();
 
-    await page.screenshot({ path: 'example.png' });
-    await browser.close();
-})();
+		await page.screenshot({ path: 'example.png' });
+		await browser.close();
+	})();
+});
